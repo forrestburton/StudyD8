691fddb7dfebfa2343ab709f846aafef5608e9ea
Forrest Burton <burton.forrest10@gmail.com>
2020-09-07 18:56:02 -0700

    Added unique username, edit profile feature, and cleaned up some UI

--- app/src/main/java/com/SAP/studyd8/ProfileEdit.java
+++ app/src/main/java/com/SAP/studyd8/ProfileEdit.java
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,13 +18,8 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.MutableData;
-import com.google.firebase.database.Transaction;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -32,30 +27,29 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+public class ProfileEdit extends AppCompatActivity {
 
-public class Profile extends AppCompatActivity {
-    EditText university, username, firstName, lastName, major, studyHabits;
+
+    EditText university, firstName, lastName, major, studyHabits;
+    TextView username;
     Button finishButton;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
-    String userId, user_username, user_firstName, user_lastName, user_major, user_university, user_studyHabits, comparison;
+    String userId, user_username, user_firstName, user_lastName, user_major, user_university, user_studyHabits;
     private DatabaseReference myRef;
     private FirebaseDatabase mFirebaseDatabase;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private DocumentReference ref;
-    boolean inUse = true;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_profile_edit);
         username = findViewById(R.id.profileUsername);
         university = findViewById(R.id.profileUniversity);
         firstName = findViewById(R.id.profileFirstName);
@@ -65,12 +59,24 @@ public class Profile extends AppCompatActivity {
 
         fStore = FirebaseFirestore.getInstance();
         fAuth = FirebaseAuth.getInstance();
-
-        if (fAuth.getCurrentUser() == null) {
-            startActivity(new Intent(getApplicationContext(), Login.class));
-            finish();
-        }
         userId = fAuth.getCurrentUser().getUid();
+
+        ref = db.collection("users").document(userId);
+
+        ref.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                username.setText(value.getString("username"));
+                firstName.setText(value.getString("firstName"));
+                lastName.setText(value.getString("lastName"));
+                university.setText(value.getString("university"));
+                major.setText(value.getString("major"));
+                studyHabits.setText(value.getString("studyHabits"));
+            }
+        });
+
+
+
         finishButton = findViewById(R.id.finishButton);
         try {
             finishButton.setOnClickListener(new View.OnClickListener() {
@@ -83,41 +89,11 @@ public class Profile extends AppCompatActivity {
                     user_university = university.getText().toString().trim();
                     user_studyHabits = studyHabits.getText().toString().trim();
 
-                    // username contains only letters and numbers
-                    user_username = user_username.replaceAll("[^a-zA-Z0-9]", "");
+                    addUserData();
 
-
-
-                    // unique username stuff - identifies if the username is unique
-                    // doesn't move on if username is not unique
-                    CollectionReference usersRef = FirebaseFirestore.getInstance().collection("users");
-                    Query query = usersRef.whereEqualTo("username", user_username);
-                    query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if(task.isSuccessful()){
-                                for(DocumentSnapshot documentSnapshot : task.getResult()){
-                                    String user = documentSnapshot.getString("username");
-
-                                    if(user.equalsIgnoreCase(user_username)){
-                                        Log.d("TAG", "User Exists");
-                                        Toast.makeText(getApplicationContext(), "Username already taken", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            }
-
-                            if(task.getResult().size() == 0 ){
-                                Log.d("TAG", "User DNE");
-                                addUserData();
-                            }
-                        }
-                    });
                 }
-
-
-
-
             });
+
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
         }
@@ -136,12 +112,10 @@ public class Profile extends AppCompatActivity {
         user_data.put("university", user_university);
         user_data.put("studyHabits", user_studyHabits);
 
-
         documentReference.set(user_data).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
-                Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), ViewProfile.class));
                 finish();
 
             }
--- app/src/main/java/com/SAP/studyd8/ViewProfile.java
+++ app/src/main/java/com/SAP/studyd8/ViewProfile.java
@@ -1,33 +1,62 @@
 package com.SAP.studyd8;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.security.keystore.UserNotAuthenticatedException;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.util.Log;
+import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUser;
 
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+
 public class ViewProfile extends AppCompatActivity {
 
     TextView topText, username, name, university, major;
     EditText studyHabits;
-    Button addCourses, submitStudy;
+    Button addCourses, submitStudy, editProfile;
     private String uid;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private DocumentReference ref;
@@ -45,6 +74,7 @@ public class ViewProfile extends AppCompatActivity {
         studyHabits = (EditText) findViewById(R.id.studyHabitsBox);
         submitStudy = (Button) findViewById(R.id.submitStudy);
         addCourses = (Button) findViewById(R.id.addCourses);
+        editProfile = (Button) findViewById(R.id.editButton);
 
         uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
@@ -69,5 +99,15 @@ public class ViewProfile extends AppCompatActivity {
                 noteRef.update("studyHabits", input);
             }
         });
+
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ProfileEdit.class));
+                finish();
+            }
+        });
+
+
     }
 }
