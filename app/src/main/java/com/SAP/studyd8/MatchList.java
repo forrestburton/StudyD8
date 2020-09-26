840878b02ab59ee33240e6781a63d756610a547c
Forrest Burton <burton.forrest10@gmail.com>
2020-09-25 17:11:36 -0700

    Added UI for match list

---
+++ app/src/main/java/com/SAP/studyd8/MatchList.java
@@ -0,0 +1,14 @@
+package com.SAP.studyd8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_list);
+    }
+}
